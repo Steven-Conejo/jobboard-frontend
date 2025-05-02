e938e5ce6f772523d97fcf245324b44303aa06ca
Fabricio Álvarez <jerry.alvarez@ucr.ac.cr>
2025-05-02 23:07:30 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jobboard-frontend',
+  webDir: 'dist'
+};
+
+export default config;
